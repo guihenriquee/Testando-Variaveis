fc9ec407fef0292d3cda7570e48293d0651af23a
Guilherme Henrique <guilhermehenriquemraissilva@gmail.com>
2024-11-09 11:53:20 -0300

    Add files via upload

---
+++ src/TestandoVariaveis.java
@@ -0,0 +1,10 @@
+public class TestandoVariaveis {
+    public static void main(String[] args) {
+        char masculino = 'M';
+        char feminino = 'F';
+        String masculino2 = "A";
+        String feminino2 = "A";
+        boolean condicao = false;
+        System.out.println("" + masculino + feminino);
+    }
+}
